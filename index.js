5324ffb35caee0e1ba3c23ac0bb6d15ea404bb7a
TahaShah141 <tahashah411@gmail.com>
2023-07-02 18:38:43 +0500

    Add reset and dimension buttons and functionality

--- index.js
+++ index.js
@@ -27,4 +27,25 @@ function getColor() {
     return "#000000";
 }
 
+document.onkeydown = function(e) {
+    if (e.code === "KeyR") resetGrid();
+    else if (e.code === "KeyN") changeDimensions();
+}
+
+function resetGrid() {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => cell.style["background-color"] = "#FFFFFF");
+        
+}
+
+function changeDimensions() {
+    let n = parseInt(prompt("Enter the dimensions of the new board"));
+    if (n > 100 || n <= 0 || !n) {alert("Dimension out of range");return;}
+
+    const rows = document.querySelectorAll(".row");
+    rows.forEach(row => row.remove());
+
+    drawGrid(n);
+}
+
 drawGrid(16);
